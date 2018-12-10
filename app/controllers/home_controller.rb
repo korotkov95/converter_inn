8051ea53dc5c8cd001264180c369f4b6cc38c44e
Vlad Korotkov <carrot2077@gmail.com>
2018-12-10 12:24:07 +0300

    update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,15 @@ class HomeController < ApplicationController
   def parse
     allinn = params[:form][:inn]
     @results = []
+    count = 1
     allinn.split("\r\n").each do |inn|
       url = "http://www.portal.nalog.gov.by/grp/getData?unp=#{inn}&charset=UTF-8"
       puts url
       response = HTTParty.get(url, format: :xml)
-      @results << Hash.from_xml(response.body)["ROWSET"]["ROW"]["VUNP"] + " - " + Hash.from_xml(response.body)["ROWSET"]["ROW"]["VKODS"]
+      inn_c = Hash.from_xml(response.body)["ROWSET"]["ROW"]["VUNP"]
+      stat_c = Hash.from_xml(response.body)["ROWSET"]["ROW"]["VKODS"]
+      @results << count.to_s + ". " + inn_c + " - " + stat_c
+      count += 1
     end
     render :index
   end
