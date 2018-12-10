a991462119d879c91bfc159db2be77c8a5a5fa7b
Vlad Korotkov <carrot2077@gmail.com>
2018-12-10 22:33:02 +0300

    adding table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,16 +4,14 @@ class HomeController < ApplicationController
 
   def parse
     allinn = params[:form][:inn]
-    @results = []
+    @results_hash = Hash.new
     count = 1
     allinn.split("\r\n").each do |inn|
       url = "http://www.portal.nalog.gov.by/grp/getData?unp=#{inn}&charset=UTF-8"
-      puts url
       response = HTTParty.get(url, format: :xml)
       inn_c = Hash.from_xml(response.body)["ROWSET"]["ROW"]["VUNP"]
       stat_c = Hash.from_xml(response.body)["ROWSET"]["ROW"]["VKODS"]
-      @results << count.to_s + ". " + inn_c + " - " + stat_c
-      count += 1
+      @results_hash.update(inn_c => stat_c)
     end
     render :index
   end
