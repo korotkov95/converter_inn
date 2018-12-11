8786aac029e719de8cd7a3f8b390cd5a279f1f2c
Vlad Korotkov <carrot2077@gmail.com>
2018-12-11 21:50:00 +0300

    exceptions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,13 +5,17 @@ class HomeController < ApplicationController
   def parse
     allinn = params[:form][:inn]
     @results_hash = Hash.new
-    count = 1
     allinn.split("\r\n").each do |inn|
-      url = "http://www.portal.nalog.gov.by/grp/getData?unp=#{inn}&charset=UTF-8"
-      response = HTTParty.get(url, format: :xml)
-      inn_c = Hash.from_xml(response.body)["ROWSET"]["ROW"]["VUNP"]
-      stat_c = Hash.from_xml(response.body)["ROWSET"]["ROW"]["VKODS"]
-      @results_hash.update(inn_c => stat_c)
+      begin
+        url = "http://www.portal.nalog.gov.by/grp/getData?unp=#{inn}&charset=UTF-8"
+        response = HTTParty.get(url, format: :xml)
+        inn_c = Hash.from_xml(response.body)["ROWSET"]["ROW"]["VUNP"]
+        stat_c = Hash.from_xml(response.body)["ROWSET"]["ROW"]["VKODS"]
+        @results_hash.update(inn_c => stat_c)
+      rescue NoMethodError
+        @results_hash.update("ошибка" => "в значении")
+        next
+      end
     end
     render :index
   end
